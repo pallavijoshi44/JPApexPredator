3f25adddc384d728a88a5ac86de1833ad2a5a25d
pallavijoshi <pallavijoshi4@gmail.com>
2024-08-19 20:09:19 +0200

    Initial Commit

---
+++ JPApexPredator/JPApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorApp.swift
+//  JPApexPredator
+//
+//  Created by Pallavi Joshi on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
